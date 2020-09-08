052eb8241a1538e8115d47a1b8b9223c67a4139a
Ricardo Henrique Comar <ricardo.comar@gmail.com>
2020-09-08 18:55:24 -0300

    swagger

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 
+http://localhost:8080/v2/api-docs
+http://localhost:8080/swagger-ui/
+
 
 https://medium.com/@tushar0618/docker-compose-to-run-and-connect-camunda-and-mysql-containers-2ac396a07d99
 https://camunda.com/download/modeler/
--- gateway/src/main/java/com/github/ricardocomar/config/SwaggerConfig.java
+++ gateway/src/main/java/com/github/ricardocomar/config/SwaggerConfig.java
@@ -2,22 +2,24 @@ package com.github.ricardocomar.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {
 
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.github.ricardocomar")).build().apiInfo(metaData());
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .build()
+                .apiInfo(metaData());
     }
 
     private ApiInfo metaData() {
@@ -26,9 +28,4 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0").build();
     }
 
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
 }

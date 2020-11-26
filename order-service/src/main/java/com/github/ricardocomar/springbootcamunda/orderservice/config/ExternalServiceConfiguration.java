b3e23d2817d4791a46d4f9ac94888d321ecae615
Ricardo Henrique Comar <ricardo.comar@gmail.com>
2020-11-26 17:11:58 -0300

    refactor external client

--- order-service/src/main/java/com/github/ricardocomar/springbootcamunda/orderservice/config/ExternalServiceConfiguration.java
+++ order-service/src/main/java/com/github/ricardocomar/springbootcamunda/orderservice/config/ExternalServiceConfiguration.java
@@ -1,36 +1,40 @@
 package com.github.ricardocomar.springbootcamunda.orderservice.config;
 
-import com.github.ricardocomar.springbootcamunda.orderservice.handler.OrderCancellHandler;
-import com.github.ricardocomar.springbootcamunda.orderservice.handler.OrderRejectHandler;
 import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExternalServiceConfiguration implements InitializingBean {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExternalServiceConfiguration.class);
+
     @Value("${camunda.engine.url}")
     private String engineUrl;
 
     @Autowired
-    private OrderCancellHandler orderCancellHandler;
-
-    @Autowired
-    private OrderRejectHandler orderRejectHandler;
+    private ApplicationContext context;
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        ExternalTaskClient client = ExternalTaskClient.create().baseUrl(engineUrl)
-                .asyncResponseTimeout(1000).build();
 
-        // subscribe to the topic
-        client.subscribe("orderCancel").handler(orderCancellHandler).open();
+        ExternalTaskClient client =
+                ExternalTaskClient.create().baseUrl(engineUrl).asyncResponseTimeout(1000).build();
 
-        // subscribe to the topic
-        client.subscribe("orderReject").handler(orderRejectHandler).open();
+        LOGGER.info("Registering External Task Clients");
+        context.getBeansOfType(ExternalTaskHandler.class).entrySet().forEach(entry -> {
+            
+            LOGGER.info("Client {} found, registering...", entry.getKey());
+            client.subscribe(entry.getKey()).handler(entry.getValue()).open();
+        });
 
     }
+
 
 }
--- payment-service/src/main/java/com/github/ricardocomar/springbootcamunda/paymentservice/config/ExternalServiceConfiguration.java
+++ payment-service/src/main/java/com/github/ricardocomar/springbootcamunda/paymentservice/config/ExternalServiceConfiguration.java
@@ -1,33 +1,38 @@
 package com.github.ricardocomar.springbootcamunda.paymentservice.config;
 
-import com.github.ricardocomar.springbootcamunda.paymentservice.handler.BankSlipValidatorHandler;
-import com.github.ricardocomar.springbootcamunda.paymentservice.handler.CardValidatorHandler;
 import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExternalServiceConfiguration implements InitializingBean {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExternalServiceConfiguration.class);
+
     @Value("${camunda.engine.url}")
     private String engineUrl;
 
     @Autowired
-    private CardValidatorHandler cardValidatorHandler;
-
-    @Autowired
-    private BankSlipValidatorHandler bankSlipValidatorHandler;
+    private ApplicationContext context;
 
     @Override
     public void afterPropertiesSet() throws Exception {
+
         ExternalTaskClient client =
                 ExternalTaskClient.create().baseUrl(engineUrl).asyncResponseTimeout(1000).build();
 
-        // subscribe to the topic
-        client.subscribe("cardValidation").handler(cardValidatorHandler).open();
-        client.subscribe("bankSlipValidation").handler(bankSlipValidatorHandler).open();
+        LOGGER.info("Registering External Task Clients");
+        context.getBeansOfType(ExternalTaskHandler.class).entrySet().forEach(entry -> {
+            
+            LOGGER.info("Client {} found, registering...", entry.getKey());
+            client.subscribe(entry.getKey()).handler(entry.getValue()).open();
+        });
 
     }
 
